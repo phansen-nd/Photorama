0c39522c2d772b0586645ba1360d00f0e1f4422c
Patrick Hansen <pat@patricks-macbook-pro-2.local>
2016-10-03 15:26:18 -0700

    Added missing file.

---
+++ Photorama/Photo+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Photo+CoreDataClass.swift
+//  Photorama
+//
+//  Created by Patrick Hansen on 10/2/16.
+//  Copyright © 2016 Patrick Hansen. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Photo: NSManagedObject {
+
+}
